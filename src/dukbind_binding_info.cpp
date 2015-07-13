6a18ebadf98234cd56d9c8c16d7b32228996b608
Julien Hamaide <julien.hamaide@fishingcactus.com>
2015-07-14 00:42:55 +0200

    Function binding info

---
+++ src/dukbind_binding_info.cpp
@@ -0,0 +1,57 @@
+#include <duktape.h>
+#include "dukbind_binding_info.h"
+#include "dukbind.h"
+#include "utils/dukbind_validation.h"
+
+#include <map>
+#include <string>
+
+namespace dukbind
+{
+    namespace internal
+    {
+        struct BindingInfoData
+        {
+            std::map<std::string,duk_c_function> //:TODO: Replace by custom collection and custom string(internalized)
+                FunctionTable;
+        };
+
+
+        BindingInfo::BindingInfo() : Data( new BindingInfoData )
+        {
+
+        }
+
+        BindingInfo::~BindingInfo()
+        {
+            delete Data;
+        }
+
+        void BindingInfo::AddFunction(
+            const char * name,
+            const duk_c_function function
+            )
+        {
+            dukbind_assert( name && name[ 0 ], "Function name cannot be empty" );
+            dukbind_assert( dukbind::validation::IsValidIdentifier( name ), "Function name should be a valid identifier" );
+            dukbind_assert( function, "Function cannot be null" );
+            auto result = Data->FunctionTable.insert( std::make_pair( std::string( name ), function ) );
+
+            dukbind_assert( result.second == true, "Function already exists" );
+        }
+
+        duk_c_function BindingInfo::GetFunction(
+            const char * name
+            ) const
+        {
+            auto it = Data->FunctionTable.find( name );
+
+            if( it != Data->FunctionTable.end() )
+            {
+                return it->second;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ tests/core/binding_info_functions.cpp
@@ -0,0 +1,52 @@
+#include <catch.hpp>
+#include <duktape.h>
+#include <dukbind.h>
+#include "dukbind_binding_info.h"
+
+static duk_ret_t dummy_function( duk_context * )
+{
+    return 0;
+}
+
+static duk_ret_t dummy_function2( duk_context * )
+{
+    return 1;
+}
+
+TEST_CASE( "Bindings handle function correctly ", "[bindings][functions]" )
+{
+    dukbind::internal::BindingInfo info;
+
+    SECTION( "Adding function with empty name asserts" )
+    {
+        REQUIRE_THROWS( info.AddFunction( "", dummy_function ) );
+        REQUIRE_THROWS( info.AddFunction( 0, dummy_function ) );
+    }
+
+    SECTION( "Adding null function asserts" )
+    {
+        REQUIRE_THROWS( info.AddFunction( "Hello", 0 ) );
+    }
+
+    SECTION( "Adding function with invalid name asserts" )
+    {
+        REQUIRE_THROWS( info.AddFunction( "123", dummy_function ) );
+        REQUIRE_THROWS( info.AddFunction( "A b", dummy_function ) );
+        REQUIRE_THROWS( info.AddFunction( "$1%", dummy_function ) );
+    }
+
+    SECTION( "Functions are queried by name" )
+    {
+        info.AddFunction( "One", dummy_function );
+        info.AddFunction( "Two", dummy_function2 );
+
+        REQUIRE( info.GetFunction( "One" ) == &dummy_function );
+        REQUIRE( info.GetFunction( "Two" ) == &dummy_function2 );
+    }
+
+    SECTION( "GetFunction returns 0 if not found" )
+    {
+        REQUIRE( info.GetFunction( "One" ) == 0 );
+        REQUIRE( info.GetFunction( "Two" ) == 0 );
+    }
+}
