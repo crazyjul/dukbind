e01481ce7a8d3b44828224f73c7ca30f28a9679f
Julien Hamaide <julien.hamaide@fishingcactus.com>
2015-07-16 08:33:43 +0200

    Stack monitor

---
+++ src/utils/dukbind_debug.h
@@ -0,0 +1,28 @@
+#pragma once
+
+namespace dukbind
+{
+    namespace debug
+    {
+        class StackMonitor
+        {
+        public:
+            StackMonitor( duk_context * ctx ) :
+                TopIndex( duk_get_top_index( ctx ) ),
+                Context( ctx )
+            {
+            }
+
+            ~StackMonitor()
+            {
+                dukbind_assert( TopIndex == duk_get_top_index( Context ), "Stack is not restored properly" );
+            }
+        private:
+
+            duk_idx_t
+                TopIndex;
+            duk_context
+                * Context;
+        };
+    }
+}
