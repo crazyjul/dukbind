59c9643dd327db9c32b5a85702edf943cebcab2f
Julien Hamaide <julien.hamaide@fishingcactus.com>
2017-11-08 15:48:29 +0100

    Fix issue with static order creation

--- src/utils/dukbind_validation.cpp
+++ src/utils/dukbind_validation.cpp
@@ -5,11 +5,16 @@ namespace dukbind
 {
     namespace validation
     {
-        static std::regex identifer_regex( "[a-zA-Z$_][a-zA-Z0-9$_]*" );
+        const std::regex & getIndentifierRegex()
+        {
+            static std::regex identifer_regex( "[a-zA-Z$_][a-zA-Z0-9$_]*" );
+
+            return identifer_regex;
+        }
 
         bool IsValidIdentifier( const char * identifier )
         {
-            return std::regex_match( identifier, identifer_regex );
+            return std::regex_match( identifier, getIndentifierRegex() );
         }
     }
 }
